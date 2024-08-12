9d064dd07592e341a2dc5b5206d86753522ccff7
Dino Gržinić <grzinicdino@gmail.com>
2024-08-12 16:02:02 +0200

    chore: remove duplicate <> brackets

--- src/components/organisms/forms/Details.tsx
+++ src/components/organisms/forms/Details.tsx
@@ -7,23 +7,21 @@ export function DetailsForm() {
   const { register } = useFormContext();
   return (
     <>
-      <>
-        <FormInput
-          id="email"
-          label="Email Address"
-          type="email"
-          {...register("email")}
-          /*defaultValue={"Your email goes here"}*/
-        />
+      <FormInput
+        id="email"
+        label="Email Address"
+        type="email"
+        {...register("email")}
+        /*defaultValue={"Your email goes here"}*/
+      />
 
-        <FormInput
-          id="phone"
-          label="Phone Number"
-          type="text"
-          {...register("phone")}
-          /*defaultValue={"Your phone number goes here"}*/
-        />
-      </>
+      <FormInput
+        id="phone"
+        label="Phone Number"
+        type="text"
+        {...register("phone")}
+        /*defaultValue={"Your phone number goes here"}*/
+      />
     </>
   );
 }
--- src/components/organisms/forms/SignUp.tsx
+++ src/components/organisms/forms/SignUp.tsx
@@ -7,33 +7,31 @@ export function SignUpForm() {
   const { register } = useFormContext();
   return (
     <>
-      <>
-        <FormInput
-          id="username"
-          label="Username"
-          type="text"
-          {...register("username", {
-            required: "Please enter your username.",
-          })}
-          /*defaultValue={"Your username goes here"}*/
-        />
+      <FormInput
+        id="username"
+        label="Username"
+        type="text"
+        {...register("username", {
+          required: "Please enter your username.",
+        })}
+        /*defaultValue={"Your username goes here"}*/
+      />
 
-        <FormInput
-          id="password"
-          label="Password"
-          type="password"
-          {...register("password")}
-          /*defaultValue={"Your password goes here"}*/
-        />
+      <FormInput
+        id="password"
+        label="Password"
+        type="password"
+        {...register("password")}
+        /*defaultValue={"Your password goes here"}*/
+      />
 
-        <FormInput
-          id="password2"
-          label="Repeat Password"
-          type="password"
-          {...register("password2")}
-          /*defaultValue="Repeat your password here"*/
-        />
-      </>
+      <FormInput
+        id="password2"
+        label="Repeat Password"
+        type="password"
+        {...register("password2")}
+        /*defaultValue="Repeat your password here"*/
+      />
     </>
   );
 }
