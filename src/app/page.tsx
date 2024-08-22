906eb335151b9e50adeb62b8ae06971f10d774d8
Dino Gržinić <grzinicdino@gmail.com>
2024-08-22 13:13:35 +0200

    Feat/form wizard (#28)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { getServerAuthSession } from "~/server/auth";
 import Users from "~/app/users/page";
+import CustomForm from "./users/testForm";
 
 const Home = async () => {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  return <Users />;
+  return (
+    <>
+      <Users />
+      <CustomForm />
+    </>
+  );
 };
 
 export default Home;
