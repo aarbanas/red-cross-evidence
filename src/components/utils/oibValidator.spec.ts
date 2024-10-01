e79efb6754a255c46aae4193dfc53e740fcd08b6
Ivan Miljancic <ivan.miljancic@gmail.com>
2024-10-01 19:25:12 +0200

    refactor(oib-validator): oib validator accepts string instead of number

--- src/components/utils/oibValidator.spec.ts
+++ src/components/utils/oibValidator.spec.ts
@@ -2,17 +2,17 @@ import isOibValid from "./oibValidator";
 
 describe("isOibValid function", () => {
   it("incorrect OIB length", () => {
-    expect(isOibValid(1)).toBe(false);
-    expect(isOibValid(1234567891012)).toBe(false);
+    expect(isOibValid("1")).toBe(false);
+    expect(isOibValid("1234567891012")).toBe(false);
   });
 
   it("valid OIB", () => {
-    expect(isOibValid(12345678903)).toBe(true);
-    expect(isOibValid(98765432106)).toBe(true);
+    expect(isOibValid("12345678903")).toBe(true);
+    expect(isOibValid("98765432106")).toBe(true);
   });
 
   it("invalid OIB", () => {
-    expect(isOibValid(12345678900)).toBe(false);
-    expect(isOibValid(98765432108)).toBe(false);
+    expect(isOibValid("12345678900")).toBe(false);
+    expect(isOibValid("98765432108")).toBe(false);
   });
 });
